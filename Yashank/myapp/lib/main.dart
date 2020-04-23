2a30ff312bfecacd4d2572604ad3ed7ba0e22435
anshuman725 <57260235+anshuman725@users.noreply.github.com>
2020-04-23 12:32:03 +0530

    Some more Changes

--- Yashank/myapp/lib/main.dart
+++ Yashank/myapp/lib/main.dart
@@ -1,169 +1,128 @@
-import 'dart:ui';
 import 'package:flutter/material.dart';
-
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key key}) : super(key: key);
-
+void main() => runApp(MYPROFILE());
+class MYPROFILE extends StatelessWidget {
+  const MYPROFILE({Key key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    
-    
-    
+  
     return MaterialApp(
-        title: "Yashank",
-        theme: ThemeData(
-           primarySwatch: Colors.green,
-        ),
-        home: Scaffold(
-          appBar: AppBar(
-            leading: IconButton(icon: Icon(Icons.menu), onPressed: (){}),
-            title: Text(
-              "My portfolio"
-            ),
-            actions: <Widget>[
-              IconButton(icon: Icon(Icons.search), onPressed: (){}),
-              IconButton(icon: Icon(Icons.person_add), onPressed: (){})
-            ],
+      title:"Anshuman",
+      theme: ThemeData(
+        primarySwatch: Colors.teal,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          leading: IconButton(icon: Icon(Icons.attach_money ), onPressed: (){print("SEND MONEY TO ME");}),
+          title: Text(
+            "MY PROFILE"
           ),
-          body: Container(
-            color:Colors.green.shade100,
-            child:SingleChildScrollView(
-              physics:BouncingScrollPhysics(),
+          actions: <Widget>[
+            IconButton(icon: Icon(Icons.call), onPressed: (){print("123456789");}),
+            IconButton(icon: Icon(Icons.contact_mail ), onPressed: (){print("sample@gmail.com");}) 
+          ],
+        ),
+        body: Container(
+          color:Colors.teal[50],
+          child:SingleChildScrollView(
+
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
                 
+                SizedBox(height:30),
                 Container(
-                  
-                  height:250.0,
-                  width:250.0,
-                  decoration:BoxDecoration(
-                    image: DecorationImage(
-                      image: NetworkImage('https://avatars2.githubusercontent.com/u/46098062?s=400&u=1770689b6f92bea5b9cc4f09bb86338c23c240d2&v=4'),
-                    ),
-                    borderRadius: BorderRadius.all(Radius.circular(125.0)),
+                  height: 225,
+                  width: 225,
+                  decoration: BoxDecoration(
+                    image:DecorationImage(
+                       image:NetworkImage("https://avatars2.githubusercontent.com/u/57260235?s=400&u=5b8b9e212a7446dad5693da84af1ad9ec4470bcb&v=4",),
+                          ),
+                    borderRadius: BorderRadius.all(Radius.circular(100.0))
+
                   ),
                 ),
-                
-                
-                
-                Padding(padding: EdgeInsetsDirectional.only(bottom:40),),
-                Text("Yashank",style: TextStyle(fontSize:40,color:Colors.deepOrangeAccent),),
-                Container(
-                  color: Colors.blue,
-                  height: 3.0,
-                  width: 100.0,
+                Padding(padding: EdgeInsetsDirectional.only(top:20),),
+                Padding(padding: EdgeInsetsDirectional.only(bottom: 40),),
+                Center(child: Text("HELLO ALL",style:TextStyle(fontSize:50,color:Colors.indigo),)),
+                SizedBox(height:30),
+                Center(child: Text("I am Anshuman",style:TextStyle(fontSize:30,color:Colors.black87),)),
+                Center(child: Text("CSE 2nd SEM",style:TextStyle(fontSize:30,color:Colors.black87),)),
+                Column(
+                  children: <Widget>[
+                    Column(
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.all(25.0),
+                          child: Text("'TALK IS CHEAP' || 'SHOW ME THE CODE'",style:TextStyle(fontSize:20,color:Colors.indigo),),
+                        ),
+                        SizedBox(height:20,),
+                          Center(child: Text("Skills",style: TextStyle(fontSize:20,color: Colors.indigo ),))
+                      ],
+                    ),
+                  ],
                 ),
                 
-                Text("CSE 4th Sem",style: TextStyle(fontSize:20,color:Colors.green.shade400,)),
-                SizedBox(height:30.0),
-                Text("MSP Alpha 2 | Dsc Android Lead",style:                 TextStyle(fontSize:20,color:Colors.deepOrangeAccent),),
-                Text("I code better with tea. Buy me a tea and will give you free advices 😉",style: TextStyle(fontSize:10,color:Colors.green.shade400)),
-                SizedBox(height:30.0),
+               SizedBox(height:20),
                 Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  mainAxisAlignment: MainAxisAlignment. spaceEvenly,
                   children: <Widget>[
-                    Text("My Flutter Class",style: TextStyle(fontSize:15,color:Colors.deepOrangeAccent),),
-                Text("Awesome Response",style: TextStyle(fontSize:15,color:Colors.green.shade400)),
-               
+                    Center(child: Text("C",style: TextStyle(fontSize:20,color: Colors.black87),)),
+                                        Center(child: Text("⭐⭐⭐",style: TextStyle(fontSize:20,color: Colors.black87),)),
+
+                    
+                     Center(child: Text("PYTHON",style: TextStyle(fontSize:20,color: Colors.black87),)),
+                                                             Row(
+                                                               children: <Widget>[
+                                                                 Center(child: Text("⭐⭐⭐",style: TextStyle(fontSize:20,color: Colors.black87),)),
+                                                               ],
+                                                             ),
+
+                     
                   ],
                 ),
-                SizedBox(height:30.0),
-                Text("My Skills", style:TextStyle(fontSize:40)),
-                SizedBox(height:15.0),
                 Column(
+                  mainAxisAlignment:MainAxisAlignment.spaceEvenly ,
+                  
                   children: <Widget>[
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children:<Widget>[
-                        Text("HTML"),
-                        Text("Pro"),
+                    SizedBox(height: 25),
+                    Card(
+                      color: Colors.lightGreen[100],
+                      margin: EdgeInsets.symmetric(horizontal:45,vertical:15),
+                    child:Padding(
+                    padding: EdgeInsets.symmetric(horizontal:30,vertical:30),
+                    child: Column(
+                      children: <Widget>[
+                      Center(child: Text("My Projects",style: TextStyle(fontSize:20,color: Colors.indigo ),)),
+                      SizedBox(height: 20),
+                                            Row(
+                                              mainAxisAlignment: MainAxisAlignment.center,
+                                              children: <Widget>[
+                                                Center(child: Text("Project 1:  ",style: TextStyle(fontSize:20,color: Colors.indigo ),)),
+                                                                      Center(child: Text("Portfolio App",style: TextStyle(fontSize:20,color: Colors.indigo ),)),
+
+                                              ],
+                                            ),
+
+                      
+                      
+
                       ],
                     ),
                     
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children:<Widget>[
-                        Text("CSS"),
-                        Text("Pro"),
-                      ],
+
+                    ),
                     ),
                   ],
                 ),
-                SizedBox(height:30.0),
-                Text("My Projects", style:TextStyle(fontSize:40)),
-                SizedBox(height:15.0),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children:<Widget>[
-                    
-                    Card(
-                      color: Colors.green,
-                      margin: EdgeInsets.symmetric(horizontal:45.0,vertical:15.0),
-                      elevation: 5,
-                      child: Padding(
-                        padding:EdgeInsets.symmetric(horizontal: 40,vertical:20),
-                        child:Column(
-                          children:<Widget>[
-                            Text("Rick and morty"),
-                            SizedBox(height:15),
-                            Image.network('https://flutter.dev/assets/flutter-lockup-c13da9c9303e26b8d5fc208d2a1fa20c1ef47eb021ecadf27046dea04c0cebf6.png'),
-                            SizedBox(height:15),
-                            Row(
-                               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              children:<Widget>[
-                                Text("Flutter"),
-                                Text("Canva"),
-                              ]
-                              
-                            ),
-                            SizedBox(height:5),
-                            Text("github.com/yashank18"),
-                            
-                          ]
-                        )
-                      ),
-                    ),
-                    Card(
-                      color: Colors.green,
-                      margin: EdgeInsets.symmetric(horizontal:45.0,vertical:15.0),
-                      elevation: 5,
-                      child: Padding(
-                        padding:EdgeInsets.symmetric(horizontal: 40,vertical:20),
-                        child:Column(
-                          children:<Widget>[
-                            Text("Dsc App"),
-                            SizedBox(height:15),
-                            Image.network('https://flutter.dev/assets/flutter-lockup-c13da9c9303e26b8d5fc208d2a1fa20c1ef47eb021ecadf27046dea04c0cebf6.png'),
-                            SizedBox(height:15),
-                            Row(
-                               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              children:<Widget>[
-                                Text("Flutter"),
-                                Text("Canva"),
-                              ]
-                              
-                            ),
-                            SizedBox(height:5),
-                            Text("github.com/Yashank18/Dsc-App"),
-                            
-                          ]
-                        )
-                      ),
-                    ),
-                    
-                  ]
-                ),
-                
-            ],
-            ),
+              ],
             ),
           ),
-          
+        
         ),
+      ),
     );
   }
 }
+
+class SingleCildScrollView {
+}
+
